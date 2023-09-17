f1f90b1a245b54b5f9c407be9fa31dd8fee40847
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-09-17 12:44:38 +0300

    Изменён tsconfig
    Теперь собранный jsx будет возвращать не теги, а React.createElement().

--- build/components/TriangleSet.js
+++ build/components/TriangleSet.js
@@ -5,8 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 function TriangleSet() {
-    return (<div>
-      <>123</>
-    </div>);
+    return (react_1.default.createElement("div", null,
+        react_1.default.createElement(react_1.default.Fragment, null, "123")));
 }
 exports.default = TriangleSet;
